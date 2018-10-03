06b30193ecdef5eef3b8fdb524a9014bee614343
Takafumi Umemoto <umecch@gmail.com>
2018-10-04 00:03:12 +0900

    /api/v1/users/me のエンドポイントを追加

--- src/main/java/jp/nmemoto/todo/api/v1/controller/UserController.java
+++ src/main/java/jp/nmemoto/todo/api/v1/controller/UserController.java
@@ -1,28 +1,39 @@
 package jp.nmemoto.todo.api.v1.controller;
 
 import jp.nmemoto.todo.api.v1.dto.UserDTO;
+import jp.nmemoto.todo.api.v1.dto.UserMeDTO;
+import jp.nmemoto.todo.domain.model.User;
 import jp.nmemoto.todo.domain.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
     private UserService userService;
+    private ModelMapper modelMapper;
 
-    @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ModelMapper modelMapper) {
         this.userService = userService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public void signUp(@RequestBody UserDTO userDTO) {
         userService.create(userDTO);
+    }
+
+    @GetMapping("/me")
+    @ResponseStatus(HttpStatus.OK)
+    public UserMeDTO findMe(@AuthenticationPrincipal(expression = "user") User user) {
+        return mapToUserMeDTO(user);
+    }
+
+    private UserMeDTO mapToUserMeDTO(User user) {
+        UserMeDTO userMeDTO = modelMapper.map(user, UserMeDTO.class);
+        return userMeDTO;
     }
 }
